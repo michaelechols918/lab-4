dcf75aed5994ea3fac1c0beb43a388b429ace2f4
Yoshishimi <michaelechols918@yahoo.com>
2016-03-22 22:27:25 -0700

    finished except five is hard

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ console.log('This is the sum() function!');
 function sum(x,y){
   var sumTotal = x + y;
   //console.log('The sum of ' + x + ' and ' + y + ' is ' + sumTotal);
-  return [sumTotal];
+  return sumTotal;
 }
 console.log('This is the multiply() function!');
 function multiply(a,b){
   var productTotal = a * b;
   //console.log ('The product of ' + a + ' and ' + b + ' is ' + productTotal);
-  return [productTotal];
+  return productTotal;
 }
 console.log('This is the sumAndMultiply() function!');
 function sumAndMultiply(m, n, o){
   var sumAnswer = sum(m, n) + o;
   var multiplyAnswer = multiply(m, n) * o;
-  console.log('The sum of ' + m + ' and ' + n + ' and ' + o + ' is ' + sumAnswer);
-  console.log('The product of ' + m + ' and ' + n + ' an ' + o + ' is ' + multiplyAnswer);
+  console.log('The numbers of ' + m + ' and ' + n + ' and ' + o + ' have a sum of ' + sumAnswer);
+  console.log('The numbers of ' + m + ' and ' + n + ' and ' + o + ' have a product of ' + multiplyAnswer);
   return [sumAnswer, multiplyAnswer];
 }
 console.log('This is the sumArray() function!');
@@ -26,7 +26,8 @@ function sumArray(thisArray){
     arrayOne = arrayOne + thisArray[i];
   }
   console.table(thisArray);
-  console.log('the sum of ' + thisArray + ' is ' + arrayOne);
+  console.log(+ m + ',' + n + ',' + o + 'were passed through an array and ' + thisArray +
+  ' is their sum');
 }
 console.log('This is the multiplyArray() function!');
 function multiplyArray(thatArray){
@@ -34,6 +35,5 @@ function multiplyArray(thatArray){
   for(var i = 1; i < thatArray.length; i++){
     arrayTwo = arrayTwo * thatArray[i];
   }
-  console.table(thatArray);
-  console.log('the product of ' + thisArray + ' is ' + arrayTwo);
-}
+console.table(thatArray);
+console.log(+ m + ',' + n + ',' + o + 'were passed through an array and ' + thatArray + ' is their product');
